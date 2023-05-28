2e1e824abc558bc12db869d80c3c1de51b413bff
SvetoslavTaterski <svetoslav0247@gmail.com>
2023-05-28 16:36:04 +0300

    Initial commit

---
+++ ASP.NET Core Introduction/ChatApp/Models/Message/MessageViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ChatApp.Models.Message
+{
+	public class MessageViewModel
+	{
+
+		public string Sender { get; set; } = null!;
+
+		public string MessageText { get; set; } = null!;
+	}
+
+	public class ChatViewModel
+	{
+		public MessageViewModel CurrentMessage { get; set; } = null!;
+
+		public List<MessageViewModel> Messages { get; set; } = null!;
+	}
+}
